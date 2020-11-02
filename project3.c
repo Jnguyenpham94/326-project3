95612286acc9f4cc8440d686496c2a3f15c3f449
Jnguyenpham94 <55006375+Jnguyenpham94@users.noreply.github.com>
2020-11-01 21:54:14 -0800

    insertion sort works need merge and threading

--- project3.c
+++ project3.c
@@ -4,7 +4,35 @@
 #include <stdlib.h>
 #include <ctype.h>
 
+int arr[] = {7, 12, 19, 3, 18, 4, 2, 6, 15, 8};// sorted: 2,3,4,6,7,8,12,15,18,19
+int SIZE = sizeof(arr) / sizeof(arr[0]);
+
+int *insertionSort(int array[])
+{
+    int val;
+    for (int i = 1; i < SIZE; i++)
+    {
+        val = array[i];
+        int j = i;
+        for (; j > 0; j--)
+        {
+            if(val < array[j-1])
+            {
+                array[j] = array[j-1];
+            }
+            else
+            {
+                break;
+            }
+            
+        }
+        array[j] = val;
+    }
+    return array;
+}
+
 int main(int argc, char *argv[])
 {
     
+    return 0;
 }
