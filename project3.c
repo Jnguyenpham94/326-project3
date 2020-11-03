33e39f67cf2052c7ebb2da30e98d4b9b1869cda8
Jnguyenpham94 <55006375+Jnguyenpham94@users.noreply.github.com>
2020-11-02 23:44:52 -0800

    odd still not there missing 1 value

--- project3.c
+++ project3.c
@@ -4,22 +4,22 @@
 #include <stdlib.h>
 #include <ctype.h>
 
-int arr[] = {7, 12, 19, 3, 18, 4, 2, 6, 15, 8}; //10 values sorted: 2,3,4,6,7,8,12,15,18,19
+//int arr[] = {7, 12, 19, 3, 18, 4, 2, 6, 15, 8}; //10 values sorted: 2,3,4,6,7,8,12,15,18,19
 //test values below:
 
 //int arr[] = {100, 7, 12, 30, 34, 19, 3, 50, 22, 18, 4, 10, 2, 40, 6, 15, 8};// 17 values sorted: 2, 3, 4, 6, 7, 8, 10, 12, 15, 18, 19, 22, 30, 34, 40, 50, 100
 
-//int arr[] = {3,2,1};//3 values: odd test Sorted: 1,2,3
+int arr[] = {3,2,1};//3 values: odd test Sorted: 1,2,3
 
 //int arr[] = {7, 12, 19, 3, 18, 4, 2, 6, 15, 8, 7, 12, 19, 3, 18, 4, 2, 6, 15, 8,7, 12, 19, 3, 18, 4, 2, 6, 15, 8,7, 12, 19, 3, 18, 4, 2, 6, 15, 8,7, 12, 19, 3, 18, 4, 2, 6, 15, 8,7, 12, 19, 3, 18, 4, 2, 6, 15, 8,7, 12, 19, 3, 18, 4, 2, 6, 15, 8,7, 12, 19, 3, 18, 4, 2, 6, 15, 8, 7, 12, 19, 3, 18, 4, 2, 6, 15, 8,7, 12, 19, 3, 18, 4, 2, 6, 15, 8};// 100 values Sorted: same as length 10 but values 10x over
 
 //test values above^
 
-#define SIZE (sizeof(arr) / sizeof(arr[0]))     //finding length of a array
-int mid = SIZE / 2;                             //middle value used for indexing
-int sortedArr[SIZE];                            //final location of sorted values
-int firstHalf[SIZE / 2];                        // stores first half of arr
-int secondHalf[SIZE / 2];                       // stores second half of arr
+#define SIZE (sizeof(arr) / sizeof(arr[0])) //finding length of a array
+int mid = SIZE / 2;                         //middle value used for indexing
+int sortedArr[SIZE];                        //final location of sorted values
+int firstHalf[SIZE / 2];                    // stores first half of arr
+int secondHalf[SIZE / 2];                   // stores second half of arr
 
 pthread_mutex_t lock;
 
@@ -76,12 +76,12 @@ void *insertionSort2(int array[])
 
 void *mergeArraysSort()
 {
-    for (int i = 0; i < SIZE/2; i++)
+    for (int i = 0; i < mid; i++)
     {
         sortedArr[i] = firstHalf[i];
     }
 
-    for (int j = 0; j < SIZE/2; j++)
+    for (int j = 0; j < mid; j++)
     {
         sortedArr[mid + j] = secondHalf[j];
     }
@@ -90,14 +90,14 @@ void *mergeArraysSort()
 
 void copyArraysEven()
 {
-    for (int i = 0; i < SIZE / 2; i++) //copies first half of array
+    for (int i = 0; i < mid; i++) //copies first half of array
     {
         firstHalf[i] = arr[i];
     }
     //firstHalf = 7, 12, 19, 3, 18
     //sorted = 3,7,12,18,19
 
-    for (int j = 0; j < SIZE / 2; j++) //copies second half of array
+    for (int j = 0; j < mid; j++) //copies second half of array
     {
         secondHalf[j] = arr[mid + j];
     }
@@ -108,7 +108,7 @@ void copyArraysEven()
 //3 2 1
 void copyArraysOdd()
 {
-    int length = mid +1;
+    int length = mid + 1;
     int length2 = mid;
 
     for (int i = 0; i < length; i++) //copies first half of array
@@ -126,9 +126,9 @@ void copyArraysOdd()
 
 int main(int argc, char *argv[])
 {
-    if(SIZE % 2 == 0)
+    if (SIZE % 2 == 0)
     {
-        copyArraysEven();//copies values from arr to 2 arrays
+        copyArraysEven(); //copies values from arr to 2 arrays
     }
     else
     {
